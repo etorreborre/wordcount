18c645fe856ee4d6ad7be9d7371aea3bf50126cf
etorreborre <etorreborre@yahoo.com>
2011-09-29 18:38:10 +1000

    moved a Timer parameter to the FilePoller class, with a default value

--- src/main/scala/reactive/FilePoller.scala
+++ src/main/scala/reactive/FilePoller.scala
@@ -2,10 +2,11 @@ package reactive
 
 import java.io.File
 
-class FilePoller(path: Signal[String]) extends Trigger {
+class FilePoller(path: Signal[String], delay: Long = 200) extends Trigger {
 
-  val lastModified = Var(new File(path.now).lastModified())
-  val timer = new Timer(0, 200, {t =>  false}) foreach { tick =>
+  private val lastModified = Var(new File(path.now).lastModified())
+
+  val timer = new Timer(0, delay, {t =>  false}) foreach { tick =>
     if (new File(path.now).lastModified() > lastModified.now) {
       lastModified() = new File(path.now).lastModified()
       source.fire(true)
