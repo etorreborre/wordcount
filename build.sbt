5c8983291448cfddf431beef9bb7c75e5bb256c5
etorreborre <etorreborre@yahoo.com>
2011-10-10 08:43:19 +1100

    the first rules are not compiling yet with actions

--- build.sbt
+++ build.sbt
@@ -23,6 +23,6 @@ libraryDependencies ++= Seq(
   "org.specs2" %% "specs2" % "1.7-SNAPSHOT",
   "org.hamcrest" % "hamcrest-all" % "1.1",
   "org.mockito" % "mockito-all" % "1.8.5",
-  "org.parboiled" % "parboiled-core" % "1.0.1",
-  "org.parboiled" % "parboiled-scala" % "1.0.1",
+  "org.parboiled" % "parboiled-core" % "1.0.1" withSources,
+  "org.parboiled" % "parboiled-scala" % "1.0.1" withSources,
   "cc.co.scala-reactive" %% "reactive-core" % "0.2-SNAPSHOT")
--- src/test/scala/essays/ParboiledTextParsingSpec.scala
+++ src/test/scala/essays/ParboiledTextParsingSpec.scala
@@ -1,19 +1,40 @@
 package essays
 
 import org.specs2._
+import org.parboiled._
+import scala.parserunners.ReportingParseRunner
+import scala.rules.{Rule2, Rule1}
+import support.Characters
+import ParboiledTextParsing.useRule1
 
 class ParboiledTextParsingSpec extends Specification with ParboiledTextParsing { def is  =
 
 "Parsers"                                                                                                                        ^
+  { page.parse("p.3") === Pages("p.3") }                                                                                         ^
                                                                                                                                  end
+
 
 }
 
-object ParboiledTextParsing extends ParboiledTextParsing
+object ParboiledTextParsing extends ParboiledTextParsing { outer =>
+  implicit def useRule1[T](rule: Rule1[T]): ReportingParserRunnerRule1[T] = new ReportingParserRunnerRule1[T](rule)
+
+  class ReportingParserRunnerRule1[T](rule: Rule1[T]) {
+    def parse(input: String) = outer.parse(rule, input)
+  }
+  def parse[T](rule: Rule1[T], input: String) = ReportingParseRunner(rule).run(input).result.get
+}
 
 trait ParboiledTextParsing extends org.parboiled.scala.Parser {
 
-  def word = rule { zeroOrMore(anyOf("a-z")) ~> ((s: String) => Results(Words.fromString(s)))}
+  def digit  = rule { "0" - "9" }
+  def digits = rule { oneOrMore(digit) }
+  
+  def pagePrefix         = rule { str("p") ~ optional(".") ~> ((s: String) => s) }
+  def page: Rule1[Pages] = rule { pagePrefix ~ digits ~> { (p: String, d: String) => Pages(p+d) } }
+
+  def letter = rule { "a" - "z" }
+  def word  = rule { zeroOrMore(letter) ~> ((s: String) => Results(Words.fromString(s)))}
   def words = rule { zeroOrMore(word) }
   def referencedText = rule { words }
 
