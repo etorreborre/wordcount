df82c6be82b92347a4aaeda79e9411da31c2f1fd
etorreborre <etorreborre@yahoo.com>
2011-10-12 10:41:10 +1100

    simplified the file polling

--- src/main/scala/reactive/FilePoller.scala
+++ src/main/scala/reactive/FilePoller.scala
@@ -4,13 +4,14 @@ import java.io.File
 
 class FilePoller(path: Signal[String], delay: Long = 200) extends Trigger {
 
-  private val lastModified = Var(new File(path.now).lastModified())
+  private var lastModified = new File(path.now).lastModified()
 
   val timer = new Timer(0, delay, {t =>  false}) foreach { tick =>
-    if (new File(path.now).lastModified() > lastModified.now) {
-      lastModified() = new File(path.now).lastModified()
+    if (new File(path.now).lastModified() > lastModified) {
+      lastModified = new File(path.now).lastModified()
       source.fire(true)
     }
   }
 }
+
 
